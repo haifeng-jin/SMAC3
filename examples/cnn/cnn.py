78859b157dda9042bf0d5e53b620ef8ce7c14939
Haifeng Jin <jin@tamu.edu>
2018-04-18 21:59:23 -0500

    cnn

---
+++ examples/cnn/cnn.py
@@ -0,0 +1,212 @@
+import datetime
+
+import keras
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import ImageDataGenerator
+
+import os
+import constant
+import GPUtil
+from keras import Input, Model
+from keras.callbacks import Callback, LearningRateScheduler, ReduceLROnPlateau
+from keras.datasets import cifar10
+from keras.layers import Conv2D, BatchNormalization, Dropout, Flatten, Dense, Activation, MaxPooling2D
+from keras.losses import categorical_crossentropy
+from keras.optimizers import Adam
+from keras.regularizers import l2
+
+
+def select_gpu():
+    try:
+        # Get the first available GPU
+        DEVICE_ID_LIST = GPUtil.getFirstAvailable()
+        DEVICE_ID = DEVICE_ID_LIST[0] # grab first element from list
+
+        # Set CUDA_VISIBLE_DEVICES to mask out all other GPUs than the first available device id
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(DEVICE_ID)
+    except EnvironmentError:
+        print("GPU not found")
+
+
+class NoImprovementError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class EarlyStop(Callback):
+    def __init__(self, max_no_improvement_num=constant.MAX_NO_IMPROVEMENT_NUM, min_loss_dec=constant.MIN_LOSS_DEC):
+        super(EarlyStop, self).__init__()
+        self.training_losses = []
+        self.minimum_loss = None
+        self._no_improvement_count = 0
+        self._max_no_improvement_num = max_no_improvement_num
+        self._done = False
+        self._min_loss_dec = min_loss_dec
+
+    def on_train_begin(self, logs=None):
+        self.training_losses = []
+        self._no_improvement_count = 0
+        self._done = False
+        self.minimum_loss = float('inf')
+
+    def on_epoch_end(self, batch, logs=None):
+        loss = logs.get('val_loss')
+        self.training_losses.append(loss)
+        if self._done and loss > (self.minimum_loss - self._min_loss_dec):
+            raise NoImprovementError('No improvement for {} epochs.'.format(self._max_no_improvement_num))
+
+        if loss > (self.minimum_loss - self._min_loss_dec):
+            self._no_improvement_count += 1
+        else:
+            self._no_improvement_count = 0
+            self.minimum_loss = loss
+
+        if self._no_improvement_count > self._max_no_improvement_num:
+            self._done = True
+
+
+def lr_schedule(epoch):
+    """Learning Rate Schedule
+
+    Learning rate is scheduled to be reduced after 80, 120, 160, 180 epochs.
+    Called automatically every epoch as part of callbacks during training.
+
+    # Arguments
+        epoch (int): The number of epochs
+
+    # Returns
+        lr (float32): learning rate
+    """
+    lr = 1e-3
+    if epoch > 180:
+        lr *= 0.5e-3
+    elif epoch > 160:
+        lr *= 1e-3
+    elif epoch > 120:
+        lr *= 1e-2
+    elif epoch > 80:
+        lr *= 1e-1
+    return lr
+
+
+class ModelTrainer:
+    """A class that is used to train model
+
+    This class can train a model with dataset and will not stop until getting minimum loss
+
+    Attributes:
+        model: the model that will be trained
+        x_train: the input train data
+        y_train: the input train data labels
+        x_test: the input test data
+        y_test: the input test data labels
+        verbose: verbosity mode
+    """
+
+    def __init__(self, model, x_train, y_train, x_test, y_test, verbose):
+        """Init ModelTrainer with model, x_train, y_train, x_test, y_test, verbose"""
+        model.compile(loss=categorical_crossentropy,
+                      optimizer=Adam(lr=lr_schedule(0)),
+                      metrics=['accuracy'])
+        self.model = model
+        self.x_train = x_train
+        self.y_train = y_train
+        self.x_test = x_test
+        self.y_test = y_test
+        self.verbose = verbose
+        if constant.DATA_AUGMENTATION:
+            self.datagen = ImageDataGenerator(
+                # set input mean to 0 over the dataset
+                featurewise_center=False,
+                # set each sample mean to 0
+                samplewise_center=False,
+                # divide inputs by std of dataset
+                featurewise_std_normalization=False,
+                # divide each input by its std
+                samplewise_std_normalization=False,
+                # apply ZCA whitening
+                zca_whitening=False,
+                # randomly rotate images in the range (deg 0 to 180)
+                rotation_range=0,
+                # randomly shift images horizontally
+                width_shift_range=0.1,
+                # randomly shift images vertically
+                height_shift_range=0.1,
+                # randomly flip images
+                horizontal_flip=True,
+                # randomly flip images
+                vertical_flip=False)
+            self.datagen.fit(x_train)
+        else:
+            self.datagen = None
+
+    def _converged(self, loss):
+        """Return whether the training is converged"""
+
+    def train_model(self):
+        """Train the model with dataset and return the minimum_loss"""
+        batch_size = min(self.x_train.shape[0], constant.MAX_BATCH_SIZE)
+        terminator = EarlyStop()
+        lr_scheduler = LearningRateScheduler(lr_schedule)
+
+        lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1),
+                                       cooldown=0,
+                                       patience=5,
+                                       min_lr=0.5e-6)
+
+        callbacks = [terminator, lr_scheduler, lr_reducer]
+        try:
+            if constant.DATA_AUGMENTATION:
+                flow = self.datagen.flow(self.x_train, self.y_train, batch_size)
+                self.model.fit_generator(flow,
+                                         epochs=constant.MAX_ITER_NUM,
+                                         validation_data=(self.x_test, self.y_test),
+                                         callbacks=callbacks,
+                                         verbose=self.verbose)
+            else:
+                self.model.fit(self.x_train, self.y_train,
+                               batch_size=batch_size,
+                               epochs=constant.MAX_ITER_NUM,
+                               validation_data=(self.x_test, self.y_test),
+                               callbacks=callbacks,
+                               verbose=self.verbose)
+        except NoImprovementError as e:
+            if self.verbose:
+                print('Training finished!')
+                print(e.message)
+
+
+def cnn(params, data):
+    w = list(map(lambda x: int(x), params[:3]))
+    d = params[3:6]
+    r = params[6:]
+
+    x_train, y_train, x_test, y_test = data
+
+    # Input image dimensions.
+    input_shape = x_train.shape[1:]
+    input_tensor = output_tensor = Input(shape=input_shape)
+    for i in range(3):
+        output_tensor = Conv2D(filters=w[i],
+                               kernel_size=3,
+                               kernel_regularizer=l2(r[i]),
+                               kernel_initializer='he_normal')(output_tensor)
+        output_tensor = BatchNormalization()(output_tensor)
+        output_tensor = Activation('relu')(output_tensor)
+        output_tensor = Dropout(d[i])(output_tensor)
+        output_tensor = MaxPooling2D()(output_tensor)
+
+    output_tensor = Flatten()(output_tensor)
+    output_tensor = Dense(10, kernel_initializer='he_normal', activation='softmax')(output_tensor)
+
+    model = Model(input_tensor, output_tensor)
+
+    x_train_new, x_val, y_train_new, y_val = train_test_split(x_train, y_train, test_size=0.25, random_state=42)
+    ModelTrainer(model, x_train_new, y_train_new, x_val, y_val, False).train_model()
+    loss, accuracy = model.evaluate(x_val, y_val, verbose=False)
+
+    print(accuracy)
+
+    return 1 - accuracy
+
