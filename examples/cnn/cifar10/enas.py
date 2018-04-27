45d7cf8d9993f9c301c88146ff02a5bef2f32dd4
Haifeng Jin <jin@tamu.edu>
2018-04-26 21:30:48 -0500

    enas

--- examples/cnn/cifar10/enas.py
+++ examples/cnn/cifar10/enas.py
@@ -138,7 +138,18 @@ def cnn(x_train, y_train, x_test, y_test):
 
     return 1 - accuracy
 
+def select_gpu():
+    try:
+        # Get the first available GPU
+        DEVICE_ID_LIST = GPUtil.getFirstAvailable()
+        DEVICE_ID = DEVICE_ID_LIST[0] # grab first element from list
 
+        # Set CUDA_VISIBLE_DEVICES to mask out all other GPUs than the first available device id
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(DEVICE_ID)
+    except EnvironmentError:
+        print("GPU not found")
+
+select_gpu()
 x_train, y_train, x_test, y_test = get_data()
 X = np.concatenate((x_train, x_test))
 Y = np.concatenate((y_train, y_test))
