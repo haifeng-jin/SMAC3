499076fb8d134aba5118c3b6be64683393ffddb5
Haifeng Jin <jin@tamu.edu>
2018-04-27 16:00:20 -0500

    enas

--- examples/cnn/cifar10/enas.py
+++ examples/cnn/cifar10/enas.py
@@ -1,10 +1,12 @@
 import os
 import GPUtil
+import tensorflow as tf
 import numpy as np
-from keras import Input, Model
-from keras.layers import SeparableConv2D, Conv2D, MaxPooling2D, Flatten, Dense, Add
+from keras import Input, Model, backend
+from keras.layers import SeparableConv2D, Conv2D, MaxPooling2D, Dense, BatchNormalization, Concatenate, \
+    GlobalAveragePooling2D
 from keras.regularizers import l2
-from sklearn.model_selection import StratifiedKFold, train_test_split
+from sklearn.model_selection import StratifiedKFold
 
 from examples.cnn.cifar10.cifar10 import get_data
 from examples.cnn.cnn import ModelTrainer
@@ -19,6 +21,7 @@ def cnn(x_train, y_train, x_test, y_test):
                                     activation='relu',
                                     padding='same',
                                     kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
     output1 = output_tensor
     output_tensor = Conv2D(256,
                            kernel_size=5,
@@ -26,6 +29,7 @@ def cnn(x_train, y_train, x_test, y_test):
                            activation='relu',
                            padding='same',
                            kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
     output2 = output_tensor
     output_tensor = Conv2D(256,
                            kernel_size=5,
@@ -33,16 +37,32 @@ def cnn(x_train, y_train, x_test, y_test):
                            activation='relu',
                            padding='same',
                            kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
     output3 = output_tensor
-    output_tensor = Add()([output_tensor, output1])
+    output_tensor = Concatenate()([output_tensor, output1])
+    output_tensor = Conv2D(256,
+                           kernel_size=1,
+                           kernel_initializer='he_normal',
+                           activation='relu',
+                           padding='same',
+                           kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
     output_tensor = SeparableConv2D(256,
                                     kernel_size=5,
                                     kernel_initializer='he_normal',
                                     activation='relu',
                                     padding='same',
                                     kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
     output4 = output_tensor
-    output_tensor = Add()([output_tensor, output1])
+    output_tensor = Concatenate()([output_tensor, output1])
+    output_tensor = Conv2D(256,
+                           kernel_size=1,
+                           kernel_initializer='he_normal',
+                           activation='relu',
+                           padding='same',
+                           kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
     output_tensor = MaxPooling2D()(output_tensor)
 
     output1 = MaxPooling2D()(output1)
@@ -56,31 +76,63 @@ def cnn(x_train, y_train, x_test, y_test):
                                     activation='relu',
                                     padding='same',
                                     kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
     output5 = output_tensor
-    output_tensor = Add()([output_tensor, output1, output4])
+    output_tensor = Concatenate()([output_tensor, output1, output4])
+    output_tensor = Conv2D(256,
+                           kernel_size=1,
+                           kernel_initializer='he_normal',
+                           activation='relu',
+                           padding='same',
+                           kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
     output_tensor = Conv2D(256,
                            kernel_size=5,
                            kernel_initializer='he_normal',
                            activation='relu',
                            padding='same',
                            kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
     output6 = output_tensor
-    output_tensor = Add()([output_tensor, output3, output4, output5])
+    output_tensor = Concatenate()([output_tensor, output3, output4, output5])
+    output_tensor = Conv2D(256,
+                           kernel_size=1,
+                           kernel_initializer='he_normal',
+                           activation='relu',
+                           padding='same',
+                           kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
     output_tensor = SeparableConv2D(256,
                                     kernel_size=3,
                                     kernel_initializer='he_normal',
                                     activation='relu',
                                     padding='same',
                                     kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
     output7 = output_tensor
-    output_tensor = Add()([output_tensor, output1, output3, output4, output5, output6])
+    output_tensor = Concatenate()([output_tensor, output1, output3, output4, output5, output6])
+    output_tensor = Conv2D(256,
+                           kernel_size=1,
+                           kernel_initializer='he_normal',
+                           activation='relu',
+                           padding='same',
+                           kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
     output_tensor = SeparableConv2D(256,
                                     kernel_size=5,
                                     kernel_initializer='he_normal',
                                     activation='relu',
                                     padding='same',
                                     kernel_regularizer=l2(1e-4))(output_tensor)
-    output_tensor = Add()([output_tensor, output1, output4, output5, output7])
+    output_tensor = BatchNormalization()(output_tensor)
+    output_tensor = Concatenate()([output_tensor, output1, output4, output5, output7])
+    output_tensor = Conv2D(256,
+                           kernel_size=1,
+                           kernel_initializer='he_normal',
+                           activation='relu',
+                           padding='same',
+                           kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
     output_tensor = MaxPooling2D()(output_tensor)
 
     output1 = MaxPooling2D()(output1)
@@ -97,48 +149,89 @@ def cnn(x_train, y_train, x_test, y_test):
                            activation='relu',
                            padding='same',
                            kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
     output9 = output_tensor
-    output_tensor = Add()([output_tensor, output6])
+    output_tensor = Concatenate()([output_tensor, output6])
+    output_tensor = Conv2D(256,
+                           kernel_size=1,
+                           kernel_initializer='he_normal',
+                           activation='relu',
+                           padding='same',
+                           kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
     output_tensor = SeparableConv2D(256,
                                     kernel_size=5,
                                     kernel_initializer='he_normal',
                                     activation='relu',
                                     padding='same',
                                     kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
     output10 = output_tensor
-    output_tensor = Add()([output_tensor, output2, output4, output7])
+    output_tensor = Concatenate()([output_tensor, output2, output4, output7])
+    output_tensor = Conv2D(256,
+                           kernel_size=1,
+                           kernel_initializer='he_normal',
+                           activation='relu',
+                           padding='same',
+                           kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
     output_tensor = SeparableConv2D(256,
                                     kernel_size=5,
                                     kernel_initializer='he_normal',
                                     activation='relu',
                                     padding='same',
                                     kernel_regularizer=l2(1e-4))(output_tensor)
-    output_tensor = Add()([output_tensor, output2, output3, output5, output9])
+    output_tensor = BatchNormalization()(output_tensor)
+    output_tensor = Concatenate()([output_tensor, output2, output3, output5, output9])
+    output_tensor = Conv2D(256,
+                           kernel_size=1,
+                           kernel_initializer='he_normal',
+                           activation='relu',
+                           padding='same',
+                           kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
     output_tensor = Conv2D(256,
                            kernel_size=3,
                            kernel_initializer='he_normal',
                            activation='relu',
                            padding='same',
                            kernel_regularizer=l2(1e-4))(output_tensor)
-    output_tensor = Add()([output_tensor, output1, output3, output4, output5, output7, output10])
+    output_tensor = BatchNormalization()(output_tensor)
+    output_tensor = Concatenate()([output_tensor, output1, output3, output4, output5, output7, output10])
+    output_tensor = Conv2D(256,
+                           kernel_size=1,
+                           kernel_initializer='he_normal',
+                           activation='relu',
+                           padding='same',
+                           kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
     output_tensor = SeparableConv2D(256,
                                     kernel_size=5,
                                     kernel_initializer='he_normal',
                                     activation='relu',
                                     padding='same',
                                     kernel_regularizer=l2(1e-4))(output_tensor)
-    output_tensor = Add()([output_tensor, output4, output7, output10])
-    output_tensor = Flatten()(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
+    output_tensor = Concatenate()([output_tensor, output4, output7, output10])
+    output_tensor = Conv2D(256,
+                           kernel_size=1,
+                           kernel_initializer='he_normal',
+                           activation='relu',
+                           padding='same',
+                           kernel_regularizer=l2(1e-4))(output_tensor)
+    output_tensor = BatchNormalization()(output_tensor)
+    output_tensor = GlobalAveragePooling2D()(output_tensor)
     output_tensor = Dense(10, activation='softmax')(output_tensor)
 
     model = Model(input_tensor, output_tensor)
 
     ModelTrainer(model, x_train, y_train, x_test, y_test, False).train_model()
-    loss, accuracy = model.evaluate(x_test, y_test, verbose=False)
+    loss, accuracy = model.evaluate(x_test, y_test, verbose=True)
 
     print(accuracy)
 
     return 1 - accuracy
+
 
 def select_gpu():
     try:
@@ -152,6 +245,14 @@ def select_gpu():
         print("GPU not found")
 
 select_gpu()
+
+config = tf.ConfigProto(allow_soft_placement=True)
+config.gpu_options.allow_growth = True
+sess = tf.Session(config=config)
+init = tf.global_variables_initializer()
+sess.run(init)
+backend.set_session(sess)
+
 x_train, y_train, x_test, y_test = get_data()
 X = np.concatenate((x_train, x_test))
 Y = np.concatenate((y_train, y_test))
